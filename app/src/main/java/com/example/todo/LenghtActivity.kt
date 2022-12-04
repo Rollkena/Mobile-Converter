fcee154d9794a688df9ca715e828b6d2d6fbd6eb
Rollkena <rollkena@gmail.com>
2022-12-05 03:28:24 +0500

    New activities

--- app/src/main/java/com/example/todo/LenghtActivity.kt
+++ app/src/main/java/com/example/todo/LenghtActivity.kt
@@ -1,11 +1,11 @@
 package com.example.todo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class ConvertActivity : AppCompatActivity() {
+class LenghtActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_convert)
+        setContentView(R.layout.activity_lenght)
     }
 }
--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.todo
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        imageTemp.setOnClickListener{
+            val intent = Intent(this, TemperatureActivity::class.java)
+            startActivity(intent);
+        }
+        imageTime.setOnClickListener{}
+        imageSpeed.setOnClickListener{}
+        imageLength.setOnClickListener{}
+        imageWeight.setOnClickListener{}
     }
 
 }
